6de95752e1b12ba8b978b8bea2b53b56cd74e673
Matthew Smilansky <msmilansky@gmail.com>
2023-07-06 16:51:31 -0700

    feat: add saved-resources.tsx

---
+++ next-app/saved-resources.tsx
@@ -0,0 +1,33 @@
+import Resource from '@/components/Home/Resource'
+
+const SavedResourceItem = ({ title, author, url }) => {
+  return (
+    <a href={url}>
+      <div className='resource'>
+        <div className='resource__title'>
+          <h3>{title}</h3>
+        </div>
+        <div className='resource__author'>
+          <h4>{author}</h4>
+        </div>
+      </div>
+    </a>
+  )
+}
+const SavedResources = () => {
+  return (
+    <main className='container'>
+      <section className='section section--resources'>
+        <header className='section--resources__header'>
+          <h2>Saved Resources</h2>
+        </header>
+        <div className='section--resources__container'>
+          <SavedResourceItem title='How to Draw Fat Yoshi in CSS' author='Fat "Beeg Boi" Yoshi' url='/saved-resource' />
+          <SavedResourceItem title='How to Eat More Food More Efficiently' author='Fat "Beeg Boi" Yoshi' url='#' />
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default SavedResources
