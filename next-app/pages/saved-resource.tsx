980eb024880457bcff1602ea5eade701b548312a
Matthew Smilansky <msmilansky@gmail.com>
2023-07-06 16:50:33 -0700

    feat: add saved-resource.tsx

---
+++ next-app/pages/saved-resource.tsx
@@ -0,0 +1,14 @@
+import Resource from '@/components/Home/Resource'
+const SavedResource = () => {
+  return (
+    <main className='container'>
+      <section className='section section--saved-resource'>
+        <header className='section--saved-resource__header'>
+          <h2>Resources for Article by Author</h2>
+        </header>
+      </section>
+    </main>
+  )
+}
+
+export default SavedResource
