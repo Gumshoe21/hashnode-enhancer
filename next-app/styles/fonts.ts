caf1d0442c58d43ec45a8c491800b982e9fd58ec
Matthew Smilansky <msmilansky@gmail.com>
2023-07-08 12:30:28 -0700

    chore: add fonts.ts file

---
+++ next-app/styles/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local'
+
+const universeTE20Thin = localFont({
+	src: './../public/fonts/UniversTE20-Thin.woff2',
+	display: 'swap',
+	variable: '--Univers'
+})
+
+export { universeTE20Thin }
