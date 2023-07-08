27bb3e6e3056f2b0dde334c991cdb9dde997b99a
Matthew Smilansky <msmilansky@gmail.com>
2023-07-08 12:31:50 -0700

    chore: add CSS global variables to _app.tsx

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '@/styles/globals.scss'
+import '@/styles/main.scss'
+import '@/styles/base/_reset.scss'
 import type { AppProps } from 'next/app'
+import { universeTE20Thin } from '@/fonts'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				:root {
+					--Univers: ${universeTE20Thin.style.fontFamily};
+				}
+			`}</style>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
