7c22eb6f52ab1a6e69d2e32a77f83ade2f3faed6
Matthew Smilansky <msmilansky@gmail.com>
2023-07-08 18:10:53 -0700

    fix: restructure the way resources are linked

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -20,43 +20,45 @@ const Home: NextPage = () => {
   }
 
   return (
-    <main className='container'>
-      {/*Begin Section Main */}
-      <section className='section section--main'>
-        <header className='section--main__header'>
-          <h1>hashnode enhancer</h1>
-        </header>
-        {/* End Section Main*/}
-      </section>
-      {/* Begin Section Recently Generated Resources */}
-      <section className='section section--resources'>
-        <header className='section--resources__header'>
-          <h2>Recent Resources</h2>
-        </header>
-        <div className='section--resources__container'>
-          <Resource title='How to Draw Fat Yoshi in CSS' author='Gum W. Shoe' />
-          <Resource title='How to Eat More Food More Efficiently' author='Fat "Beeg Boi" Yoshi' />
-          {/* End Section Recent Resources */}
-        </div>
-      </section>
-      {/* Begin Grid*/}
-      <section className='section section--grid'>
-        <div className='section--grid__container'>
-          <div className='section--grid__item'>
-            <a href='/saved-resources'> Your Resources</a>
-          </div>
-          <div className='section--grid__item'>
-            <a href='/reading-history'> Reading History</a>
-          </div>
-          <div className='section--grid__item'>
-            <a href='https://www.hashnode.com/explore'>Explore</a>
-          </div>
-          <div className='section--grid__item'>Share</div>
-        </div>
-      </section>
-      <section className='section section--generate-resources'></section>
-    </main>
-  )
+		<main className='container'>
+			{/*Begin Section Main */}
+			<section className='section section--main'>
+				<header className='section--main__header'>
+					<h1>hashnode enhancer</h1>
+				</header>
+				{/* End Section Main*/}
+			</section>
+			{/* Begin Section Recently Generated Resources */}
+			<section className='section section--resources'>
+				<header className='section--resources__header'>
+					<h2>recent resources</h2>
+				</header>
+				<div className='section--resources__container'>
+					<Resource title='How to Draw Fat Yoshi in CSS' author='Gum W. Shoe' />
+					<Resource title='How to Eat More Food More Efficiently' author='Fat "Beeg Boi" Yoshi' />
+					{/* End Section Recent Resources */}
+				</div>
+			</section>
+			{/* Begin Grid*/}
+			<section className='section section--grid'>
+				<div className='section--grid__container'>
+					<a className='section--grid__item fill fill--top-left' href='https://www.hashnode.com/explore'>
+						<div>your resources</div>
+					</a>
+					<a className='section--grid__item fill fill--top-right' href='https://www.hashnode.com/explore'>
+						<div>share</div>
+					</a>
+					<a className='section--grid__item fill fill--bottom-left' href='https://www.hashnode.com/explore'>
+						<div>reading history</div>
+					</a>
+					<a className='section--grid__item fill fill--bottom-right' href='https://www.hashnode.com/explore'>
+						<div>explore</div>
+					</a>
+				</div>
+			</section>
+			<section className='section section--generate-resources'></section>
+		</main>
+	)
 }
 
 export default Home
